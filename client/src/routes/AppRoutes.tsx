c36eea2cf4f84c068e6d537f33164b3d30f3f9f8
aoluoch <aoluoch271@gmail.com>
2025-10-10 07:48:05 +0300

    forgot password page

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -11,6 +11,8 @@ import ResidentLayout from '../layouts/ResidentLayout'
 // Auth Pages
 import Login from '../pages/auth/Login'
 import Register from '../pages/auth/Register'
+import ForgotPassword from '../pages/auth/ForgotPassword'
+import ResetPassword from '../pages/auth/ResetPassword'
 
 // Admin Pages
 import AdminDashboard from '../pages/admin/Dashboard'
@@ -51,6 +53,8 @@ const AppRoutes: React.FC = () => {
       <Route path="/auth" element={<AuthLayout />}>
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="reset-password" element={<ResetPassword />} />
       </Route>
 
       {/* Admin Routes */}
